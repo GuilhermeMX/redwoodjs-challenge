77be1bff866846f801c3081f32e88b8e747017f6
Guilherme M. Xavier <100965793+GuilhermeMX@users.noreply.github.com>
2023-11-20 00:57:18 -0300

    fixed graphql middleware

--- api/src/functions/graphql.js
+++ api/src/functions/graphql.js
@@ -1,4 +1,7 @@
-import { createGraphQLHandler } from '@redwoodjs/graphql-server'
+import {
+  createGraphQLHandler,
+  createGraphQLMiddleware,
+} from '@redwoodjs/graphql-server'
 
 import directives from 'src/directives/**/*.{js,ts}'
 import sdls from 'src/graphql/**/*.sdl.{js,ts}'
@@ -6,6 +9,24 @@ import services from 'src/services/**/*.{js,ts}'
 
 import { db } from 'src/lib/db'
 import { logger } from 'src/lib/logger'
+import { uploadFile } from 'src/services/upload'
+
+const uploadMiddleware = createGraphQLMiddleware({
+  context: async ({ context, request }) => {
+    if (request.method === 'POST' && request.path === '/api/upload') {
+      try {
+        const result = await uploadFile({ request })
+        context.uploadResult = result
+      } catch (error) {
+        console.error('Erro ao salvar o arquivo no banco de dados:', error)
+        context.uploadResult = {
+          success: false,
+          message: 'Erro ao salvar o arquivo no banco de dados.',
+        }
+      }
+    }
+  },
+})
 
 export const handler = createGraphQLHandler({
   loggerConfig: { logger, options: {} },
@@ -16,4 +37,6 @@ export const handler = createGraphQLHandler({
     // Disconnect from your database with an unhandled exception.
     db.$disconnect()
   },
+
+  middlewares: [uploadMiddleware],
 })
